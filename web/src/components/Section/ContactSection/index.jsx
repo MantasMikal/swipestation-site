4c6d399377280538d5162a655ad08df966b9a52f
MantasMikal <mikalauskas.it@gmail.com>
2020-08-24 18:11:54 +0300

    feature(Contact): Adds contact form

--- web/src/components/Section/ContactSection/index.jsx
+++ web/src/components/Section/ContactSection/index.jsx
@@ -4,9 +4,15 @@ import { array, string } from 'prop-types'
 import BlockContent from '../../Editor'
 import Container from 'Primitive/Container'
 import Type from 'Primitive/Type'
+import TextControl from 'Primitive/TextControl'
+import FieldTemplate from 'Primitive/FieldTemplate'
+import ButtonStandard from 'Primitive/ButtonStandard'
 
 import styles from './ContactSection.module.scss'
 
+/**
+ * If deployed on Netlify the form will be handled automagically! Just check the 'Forms' section under site settings
+ */
 const ContactSection = ({ body, title }) => {
   return (
     <Container size="wide" center gutter spacious withNavSpace as="section">
@@ -18,6 +24,35 @@ const ContactSection = ({ body, title }) => {
           <BlockContent blocks={body} />
         </div>
       )}
+      <form
+        name="contact"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        className={styles.Form}
+      >
+        <input type="hidden" name="form-name" value="contact" />
+        <FieldTemplate label="Name" required controlName="name">
+          <TextControl name="name" type="text" required />
+        </FieldTemplate>
+        <FieldTemplate label="Email" required controlName="email">
+          <TextControl name="email" type="email" required />
+        </FieldTemplate>
+        <FieldTemplate label="Message" required controlName="message">
+          <TextControl
+            name="message"
+            placeholder="Say hi!"
+            multiLine
+            rows={10}
+            required
+          />
+        </FieldTemplate>
+        <ButtonStandard className={styles.ApplyButton} type="submit">
+          <Type size="base" demi>
+            Apply
+          </Type>
+        </ButtonStandard>
+      </form>
     </Container>
   )
 }
