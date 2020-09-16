cf2c4d5894b576623da73750745ed87041ce11d6
MantasMikal <mikalauskas.it@gmail.com>
2020-09-16 20:22:16 +0100

    Revert "feat: use css instead of js for animations"
    This reverts commit 16111d72b3ae59b2dde52e5c6fbd31fc8c5474a8.

--- web/src/components/Common/Diagram/index.jsx
+++ web/src/components/Common/Diagram/index.jsx
@@ -1,17 +1,14 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { node } from 'prop-types'
-import classNames from 'classnames'
-
+import { useSpring, animated } from 'react-spring'
 import useOnScreen from 'libs/use-on-screen'
+
 import Type from 'Primitive/Type'
 
 import styles from './Diagram.module.scss'
 
-const BarBlock = ({ number, width, color, style }) => (
-  <div
-    className={styles.Bar}
-    style={{ width: width, backgroundColor: color, ...style }}
-  >
+const BarBlock = ({ number, width, color }) => (
+  <div className={styles.Bar} style={{ width: width, backgroundColor: color }}>
     <Type className={styles.Number} size="displayTiny">
       {number}
     </Type>
@@ -21,18 +18,26 @@ const BarBlock = ({ number, width, color, style }) => (
   </div>
 )
 
-const Bar = ({ bars, label, delay, shouldAnimate }) => {
-  const [isDone, setIsDone] = useState(false)
+const Bar = ({ bars, label, delay, shouldAnimate, onFinish }) => {
+  const bar = useSpring({
+    from: {
+      transform: 'translate(-100%)'
+    },
+    to: {
+      transform: 'translate(0%)'
+    },
+    delay: delay,
+    config: { mass: 1, tension: 280, friction: 80, precision: 0.001 }
+  })
+
+  useEffect(() => {
+    if (shouldAnimate) {
+      onFinish && onFinish()
+    }
+  }, [shouldAnimate, onFinish])
+
   return (
-    <div
-      className={classNames(
-        styles.BarWrapper,
-        shouldAnimate && styles.animate,
-        isDone && styles.isDone
-      )}
-      style={{ animationDelay: `${delay}ms` }}
-      onAnimationEnd={() => setIsDone(true)}
-    >
+    <animated.div style={bar} className={styles.BarWrapper}>
       {bars.map((bar, i) => (
         <BarBlock
           key={`${label}-${i}`}
@@ -44,80 +49,94 @@ const Bar = ({ bars, label, delay, shouldAnimate }) => {
       <Type as="label" size="displayTiny" className={styles.DiagramLabel}>
         {label}
       </Type>
-    </div>
+    </animated.div>
   )
 }
 
 const Diagram = () => {
   const diagramRef = useRef()
   const onScreen = useOnScreen(diagramRef, '-7%')
+  const [isDone, setIsDone] = useState(false)
 
   return (
     <div ref={diagramRef} className={styles.Diagram}>
       <div className={styles.Wrapper}>
-        <Bar
-          bars={[{ number: 15, width: '25%', color: '#ffba00' }]}
-          label="Swipestation"
-          delay={0}
-          shouldAnimate={onScreen}
-        />
+        {(isDone || onScreen) && (
+          <Bar
+            bars={[{ number: 15, width: '25%', color: '#ffba00' }]}
+            label="Swipestation"
+            delay={0}
+            shouldAnimate={onScreen}
+            onFinish={() => setIsDone(true)}
+          />
+        )}
       </div>
 
       <div className={styles.Wrapper}>
-        <Bar
-          bars={[
-            { number: 15, width: '25%', color: '#ffba00' },
-            { number: 10, width: '12%', color: '#A4EDFF' }
-          ]}
-          label="Free Bar!"
-          delay={250}
-          shouldAnimate={onScreen}
-        />
+        {(isDone || onScreen) && (
+          <Bar
+            bars={[
+              { number: 15, width: '25%', color: '#ffba00' },
+              { number: 10, width: '12%', color: '#A4EDFF' }
+            ]}
+            label="Free Bar!"
+            delay={250}
+            shouldAnimate={onScreen}
+          />
+        )}
       </div>
 
       <div className={styles.Wrapper}>
-        <Bar
-          bars={[
-            { number: 15, width: '25%', color: '#ffba00' },
-            { number: 15, width: '25%', color: '#CE74BF' }
-          ]}
-          label="Other mobile Payment providers"
-          delay={500}
-          shouldAnimate={onScreen}
-        />
+        {(isDone || onScreen) && (
+          <Bar
+            bars={[
+              { number: 15, width: '25%', color: '#ffba00' },
+              { number: 15, width: '25%', color: '#CE74BF' }
+            ]}
+            label="Other mobile Payment providers"
+            delay={500}
+            shouldAnimate={onScreen}
+          />
+        )}
       </div>
 
       <div className={styles.Wrapper}>
-        <Bar
-          bars={[
-            { number: 15, width: '25%', color: '#ffba00' },
-            { number: 10, width: '12%', color: '#E6E6E6' },
-            { number: 15, width: '25%', color: '#CE74BF' },
-            { number: 10, width: '12%', color: '#A4EDFF' }
-          ]}
-          label="Contactless"
-          delay={750}
-          shouldAnimate={onScreen}
-        />
+        {(isDone || onScreen) && (
+          <Bar
+            bars={[
+              { number: 15, width: '25%', color: '#ffba00' },
+              { number: 10, width: '12%', color: '#E6E6E6' },
+              { number: 15, width: '25%', color: '#CE74BF' },
+              { number: 10, width: '12%', color: '#A4EDFF' }
+            ]}
+            label="Contactless"
+            delay={750}
+            shouldAnimate={onScreen}
+          />
+        )}
       </div>
 
       <div className={styles.Wrapper}>
-        <Bar
-          bars={[
-            { number: 15, width: '25%', color: '#ffba00' },
-            { number: 20, width: '30%', color: '#E6E6E6' },
-            { number: 15, width: '25%', color: '#CE74BF' },
-            { number: 10, width: '12%', color: '#A4EDFF' }
-          ]}
-          label="Cash"
-          delay={1000}
-          shouldAnimate={onScreen}
-        />
+        {(isDone || onScreen) && (
+          <Bar
+            bars={[
+              { number: 15, width: '25%', color: '#ffba00' },
+              { number: 20, width: '30%', color: '#E6E6E6' },
+              { number: 15, width: '25%', color: '#CE74BF' },
+              { number: 10, width: '12%', color: '#A4EDFF' }
+            ]}
+            label="Cash"
+            delay={1000}
+            shouldAnimate={onScreen}
+          />
+        )}
       </div>
     </div>
   )
 }
 
-Diagram.propTypes = {}
+Diagram.propTypes = {
+  children: node.isRequired
+}
 
 export default Diagram
