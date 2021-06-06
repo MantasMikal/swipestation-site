1c11360daab504c48f61b582b9fe39adf5e1a229
MantasMikal <mantas.codes@gmail.com>
2021-06-06 21:32:19 +0100

    fix: deployments from CMS

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -9,7 +9,7 @@ export default {
             title: "Deploy Swipestation",
             apiId: "2360ecf2-68e4-49a4-b6b1-0c05c9f67014",
             buildHookId: "60532f38569a0ac3d863afd9",
-            name: "swipestation",
+            name: "distracted-northcutt-4e7b6e",
           },
         ],
       },
