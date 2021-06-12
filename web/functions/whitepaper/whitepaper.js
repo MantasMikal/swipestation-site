24d22355a7948d467c62755d0919a1a38b1ca1a9
MantasMikal <mantas.codes@gmail.com>
2021-06-12 10:25:06 +0100

    attempt to fix mailer

--- web/functions/whitepaper/whitepaper.js
+++ web/functions/whitepaper/whitepaper.js
@@ -3,11 +3,14 @@ const nodemailer = require('nodemailer')
 const config = require('../../../config')
 const path = require('path')
 const pug = require('pug')
+const templ = require('./whitepaper.pug')
+var fs = require("fs");
 
 exports.handler = async function (event) {
   const { email, emailBody, subject, attachment, title } = JSON.parse(
     event.body
   )
+
   const attachmentUrl = attachment.asset.url
 
   const options = {
@@ -23,7 +26,10 @@ exports.handler = async function (event) {
 
 
   try {
-    let template = pug.renderFile('whitepaper.pug', {
+    var text = fs.readFileSync("./whitepaper.pug");
+    console.log("🚀 ~ file: whitepaper.js ~ line 14 ~ text", text)
+    console.log('templ: ', templ)
+    let template = pug.renderFile('./whitepaper.pug', {
       siteUrl: config.site.siteUrl,
       emailBody: emailBody,
       attachmentUrl: attachmentUrl,
